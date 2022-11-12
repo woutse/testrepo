e02ab5ea742c6d1308bd3214d4ecf223a5aa75ee
woutse <wouter@pay.nl>
2022-11-12 11:39:50 +0100

    test

--- libraries/MyClass.php
+++ libraries/MyClass.php
@@ -6,11 +6,13 @@ class MyClass
 
     private string $spaak = 'yj';
 
+
     /**
      * MyClass constructor.
      * @param string|null $spaak
+     * @param int $getal
      */
-    public function __construct(string $spaak = null, $getal = 2)
+    public function __construct(string $spaak = null, int $getal = 2)
     {
 
         if (!empty($spaak)) {
